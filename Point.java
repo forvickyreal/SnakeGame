259cae7e7bec8e0cff8ec1cdaa8ad80355b19494
forvickyreal <69677129+forvickyreal@users.noreply.github.com>
2022-11-17 21:05:26 +0300

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,20 @@
+public class Point { private int x, y;
+public x=0;
+y=0;} public this.x
+this.y
+public this.x
+public this.y
+Point() {
+Point(int x, int y) { = x;
+= y;
+void setX(int x) { = x;
+void setY(int y) { = y;
+}
+}
+}
+public
+return this.x;
+int getX() {
+}
+public int getY() {
+return this.y; }}
